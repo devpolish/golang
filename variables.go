eb14cd2d8a2052d89e99d6db4a30e3544f245497
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-17 06:27:09 -0500

    beginners algorithms

---
+++ variables.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main() {
+  var x, y, z = 20.0, 40, "example"
+  fmt.Printf("x is of type %T\n", x)
+  fmt.Printf("y is of type %T\n", y)
+  fmt.Printf("z is of type %T\n", z)
+}
